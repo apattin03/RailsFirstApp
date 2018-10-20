9e80f395542150bfc374463da81dc866cd5c04cf
Alec Pattin <apattin03@gmail.com>
2018-10-20 15:54:01 -0500

    Added login features with validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+
+
+  get 'sessions/new'
+
+
+
+  resources :posts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'application#hello'
+
+  get  'static_pages/home'
+  get 'layouts/application'
+  root 'static_pages#home'
+
+
+  root 'static_pages#home'
+
+  get  '/contact', to: 'static_pages#contact'
+
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get '/logout',  to: 'sessions#destroy'
+
+  get  '/signup',  to: 'users#new'
 end
