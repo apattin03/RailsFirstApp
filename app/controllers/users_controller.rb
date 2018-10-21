f6d92144b159dbe7541dc04c31bb053703ad420d
Alec Pattin <apattin03@gmail.com>
2018-10-21 11:58:44 -0500

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the My Ruby App!"
-      redirect_back_or user
+      render 'show'
     else
       render 'new'
     end
